bbd4e59700ce795e0d679139195764c0bf038285
Redlnn <41742767+Redlnn@users.noreply.github.com>
2021-08-11 21:25:49 +0800

    [perf]Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@
 
 import logging
 
-from graia.application.entry import (GraiaMiraiApplication, Group, MessageChain, Plain)
+from graia.application.entry import (GraiaMiraiApplication, Group, Friend, MessageChain, Plain, App)  # noqa
 
 from miraibot import GetCore
 
@@ -21,11 +21,25 @@ logger = logging.getLogger(f'MiraiBot.{__name__}')
 async def group_message_listener(app: GraiaMiraiApplication, group: Group, message: MessageChain):
     logger.info(f'接收到的消息链为: ↓\n{message}')
 
-    cmd: str = message.asDisplay().strip()  # 如 "!test a bc 3 4d"
-    if cmd[0] not in ('!', '！'):
-        return 0
-    args: list = cmd[1:].strip().split()  # 切割命令，结果为 ('test', 'a', 'bc', '3', '4d')
-    logger.info(f'处理后的命令为: ↓\n{args}')
+    # cmd: str = message.asDisplay().strip()  # 如 "!test a bc 3 4d"
+    # if cmd[0] not in ('!', '！'):
+    #     return 0
+    # args: list = cmd[1:].strip().split()  # 切割命令，结果为 ('test', 'a', 'bc', '3', '4d')
+    # logger.info(f'处理后的命令为: ↓\n{args}')
 
-    message_id = await app.sendGroupMessage(group, MessageChain.create([Plain('Test')]))
-    logger.info(f'本次发送的消息的ID为：{message_id}')
+    # message_id = await app.sendGroupMessage(group, MessageChain.create([Plain('Test')]))
+    # logger.info(f'本次发送的消息的ID为：{message_id}')
+
+
+@bcc.receiver("FriendMessage")
+async def friend_message_listener(app: GraiaMiraiApplication, friend: Friend, message: MessageChain):
+    logger.info(f'接收到的消息链为: ↓\n{message}')
+
+    # cmd: str = message.asDisplay().strip()  # 如 "!test a bc 3 4d"
+    # if cmd[0] not in ('!', '！'):
+    #     return 0
+    # args: list = cmd[1:].strip().split()  # 切割命令，结果为 ('test', 'a', 'bc', '3', '4d')
+    # logger.info(f'处理后的命令为: ↓\n{args}')
+
+    # message_id = await app.sendGroupMessage(group, MessageChain.create([Plain('Test')]))
+    # logger.info(f'本次发送的消息的ID为：{message_id}')
