922f3dfad8d2141d562069848e5b39e7a8bd863f
Redlnn <41742767+Redlnn@users.noreply.github.com>
2021-08-12 16:56:33 +0800

    [perf]修改自动回复插件的描述

--- AutoReply/info.py
+++ AutoReply/info.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 MODULE_NAME = '自动回复-精确匹配'
-MODULE_DESC = '根据关键词自动回复文字，不支持发送图片或表情，可把长文本转换为图片后再发送'
+MODULE_DESC = '根据关键词自动回复文字，不支持发送自定义图片或表情，可把长文本转换为图片后再发送'
 MODULE_AUTHOR = 'Red_lnn'
 MODULE_AUTHOR_CONTACT = 'https://github.com/Redlnn'
 
