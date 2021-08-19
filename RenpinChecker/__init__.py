207010bc1bb22c7e39dc0a90c102c25aee023bae
Redlnn <41742767+Redlnn@users.noreply.github.com>
2021-08-19 17:06:09 +0800

    [perf]增加注释

--- RenpinChecker/__init__.py
+++ RenpinChecker/__init__.py
@@ -8,12 +8,12 @@ import os
 import random
 
 import yaml as yml
-from graia.application.entry import (GraiaMiraiApplication, Group, MessageChain, Member, Plain, Source, At)  # noqa
+from graia.application.entry import (GraiaMiraiApplication, Group, MessageChain, Member, Plain, Source, At)
 
 from miraibot import GetCore
 
 MODULE_NAME = '人品测试'
-MODULE_DESC = '每个QQ号每天可随机获得一个0-100的整数（人品值），在当天内该值不会改变'
+MODULE_DESC = '每个QQ号每天可随机获得一个0-100的整数（人品值），在当天内该值不会改变，该值会存放于一yml文件中，每日删除过期文件'
 MODULE_AUTHOR = 'Red_lnn'
 MODULE_AUTHOR_CONTACT = 'https://github.com/Redlnn'
 
@@ -30,31 +30,37 @@ data_folder = os.path.join(os.path.dirname(__file__), 'data')
 
 
 def del_outdated_data(date: str) -> None:
+    """
+    删除过时的数据文件
+    """
     for _ in os.listdir(data_folder):
         if _ != f'data_{date}.yml':
             os.remove(os.path.join(data_folder, _))
 
 
 def read_data(date: str, qq: int) -> int:
+    """
+    在文件中读取指定QQ今日已生成过的随机数，若今日未生成，则新生成一个随机数并写入文件
+    """
     del_outdated_data(date)
     data_file_path = os.path.join(data_folder, f'data_{date}.yml')
     if not os.path.exists(data_folder):
-        os.mkdir(data_folder)
+        os.mkdir(data_folder)  # 如果同级目录不存在data文件夹，则新建一个
     if os.path.isfile(data_folder):
         os.remove(data_folder)
-        os.mkdir(data_folder)
+        os.mkdir(data_folder)  # 如果同级目录存在data文件，则删除该文件后新建一个同名文件夹
 
-    with open(data_file_path, 'a+', encoding='utf-8') as f:
-        f.seek(0, 0)
-        yml_data: dict = yml.safe_load(f)
-        f.seek(0, 2)
-        if yml_data is None:
+    with open(data_file_path, 'a+', encoding='utf-8') as f:  # 以 追加+读 的方式打开文件
+        f.seek(0, 0)  # 将读写指针放在文件头部
+        yml_data: dict = yml.safe_load(f)  # 读写
+        f.seek(0, 2)  # 将读写指针放在文件尾部
+        if yml_data is None:  # 若文件为空，则生成一随机数并写入到文件中，然后返回生成的随机数
             random_int = random.randint(0, 100)
             yml.dump({str(qq): random_int}, f, allow_unicode=True)
             return random_int
-        if str(qq) in yml_data.keys():
+        if str(qq) in yml_data.keys():  # 若文件中有指定QQ的数据则读取并返回
             return yml_data[str(qq)]
-        else:
+        else:  # 若文件中没有指定QQ的数据，则生成一随机数并写入到文件中，然后返回生成的随机数
             random_int = random.randint(0, 100)
             yml.dump({str(qq): random_int}, f, allow_unicode=True)
             return random_int
@@ -65,8 +71,8 @@ async def group_message_listener(app: GraiaMiraiApplication, group: Group, messa
     if group.id not in active_group and active_group:
         return None
 
-    if message.asDisplay().lower() in ('.jrrp', '!jrrp'):
-        date_today = datetime.datetime.now().strftime('%Y-%m-%d')
+    if message.asDisplay().lower() in ('.jrrp', '!jrrp'):  # 可用命令：.jrrp !jrrp ，即 JinRiRenPin
+        date_today = datetime.datetime.now().strftime('%Y-%m-%d')  # 获得今日日期
         jrrp = read_data(date_today, member.id)
 
         await app.sendGroupMessage(group, MessageChain.create([
