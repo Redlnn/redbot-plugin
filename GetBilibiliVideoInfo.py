ded195a88a285f2731f36d2be7fffdd68be88a20
Kaedehara Kazuha <w731347477@gmail.com>
2021-11-02 19:41:20 +0800

    Update GetBilibiliVideoInfo.py

--- GetBilibiliVideoInfo.py
+++ GetBilibiliVideoInfo.py
@@ -74,12 +74,12 @@ async def get_video_info(origin_id: str = None, app: GraiaMiraiApplication = Non
 
     video_cover_url: str = video_info['pic']  # 封面地址
     video_bvid: str = video_info['bvid']  # BV号
-    video_avid: int = video_info['aid']  # av号
+    # video_avid: int = video_info['aid']  # av号
     video_title: str = video_info['title']  # 视频标题
     video_sub_num: int = video_info['videos']  # 视频分P数
     video_pub_date: str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(video_info['pubdate']))  # 视频发布时间(时间戳)
     # video_unload_date: int = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(video_info['ctime']))  # 视频上传时间(时间戳)
-    video_desc: str = video_info['desc']  # 视频简介
+    video_desc: str = regex.sub(r'(\n)+', '\n', video_info['desc'], flags=regex.MULTILINE)  # 视频简介，去除多余空行
     video_duration_s: int = video_info['duration']  # 视频长度（单位：秒）
     video_length_m, video_length_s = divmod(video_duration_s, 60)  # 将总的秒数转换为时分秒格式
     video_length_h, video_length_m = divmod(video_length_m, 60)
@@ -95,13 +95,15 @@ async def get_video_info(origin_id: str = None, app: GraiaMiraiApplication = Non
     video_coin: int = video_info['stat']['coin']  # 投币量
     video_favorite: int = video_info['stat']['favorite']  # 收藏量
 
-    video_desc: str = video_desc.split('\n', 1)[0]  # 简介只取第一行，提前处理方便格式化
+    if len(video_desc.strip()) > 0:
+        video_desc: str = '\n | ' + video_desc.replace('\n', '\n | ')
+    else:
+        video_desc: str = '-'
 
     info_text = f'''\
 BV号：{video_bvid}
-av号：av{video_avid}
-标题：{video_title[:20] + "..." if len(video_title) > 20 else video_title}
-简介：{video_desc[:25] + "..." if len(video_desc) > 25 else video_desc}
+标题：{video_title[:30] + "..." if len(video_title) > 30 else video_title}
+简介：{video_desc[:40] + "..." if len(video_desc) > 40 else video_desc}
 UP主：{video_up_name}
 时长：{video_length}
 发布时间：{video_pub_date}
@@ -131,7 +133,7 @@ async def register_command_2():
 @bcc.receiver('GroupMessage')
 async def group_message_listener(app: GraiaMiraiApplication, group: Group, message: MessageChain):
     if group.id not in active_group and active_group:
-        return None
+        return
     origin_id: str = None
     if message.has(App):  # noqa
         app_json = message.get(App)[0].content  # noqa
@@ -142,21 +144,21 @@ async def group_message_listener(app: GraiaMiraiApplication, group: Group, messa
             try:
                 app_id = app_dict['meta']['news']['appid']
             except:  # noqa
-                return None
+                return
         except:  # noqa
-            return None
+            return
 
         if int(app_id) == 1109937557:
             b23_url = app_dict['meta']['detail_1']['qqdocurl']
         elif int(app_id) == 1105517988:
             b23_url = app_dict['meta']['news']['jumpUrl']
         else:
-            return None
+            return
         res = requests.get(b23_url, allow_redirects=False)
         bli_url = res.headers['Location']
         origin_id = regex.search(bvid_re, bli_url)  # 获得BV号
         if origin_id is None:
-            return None
+            return
         else:
             origin_id = origin_id.group(0)  # noqa
     elif message.has(Xml):  # noqa
@@ -176,23 +178,23 @@ async def group_message_listener(app: GraiaMiraiApplication, group: Group, messa
                 bli_url = res.headers['Location']
                 origin_id = regex.search(bvid_re, bli_url)  # 获得BV号
                 if origin_id is None:
-                    return None
+                    return
                 else:
                     origin_id = origin_id.group(0)  # noqa
             else:
-                return None
+                return
         else:
-            return None
+            return
     elif message.has(Plain):  # noqa
-        cmd: str = message.asDisplay().strip()  # 如 "!BV1S64y1W7ej" 或 "!av762147945"
+        cmd: str = message.asDisplay().strip()  # 如 "BV1S64y1W7ej" 或 "av762147945"
         if len(cmd) == 0:
-            return None
+            return
         if 'www.bilibili.com/video/' in cmd:
             origin_id = regex.search(bvid_re, cmd)  # 获得BV号
             if origin_id is None:
                 origin_id = regex.search(avid_re, cmd)  # 获得BV号
             if origin_id is None:
-                return None
+                return
             origin_id = origin_id.group(0)  # noqa
         elif 'b23.tv/' in cmd:
             b23_url = regex.search('b23.tv/[0-9a-zA-Z]*', cmd).group(0)  # 去除多余的类似 "?shareFrom=xxx" 的内容
@@ -200,27 +202,23 @@ async def group_message_listener(app: GraiaMiraiApplication, group: Group, messa
             bli_url = res.headers['Location']
             origin_id = regex.search(bvid_re, bli_url)  # 获得BV号
             if origin_id is None:
-                return None
+                return
             else:
                 origin_id = origin_id.group(0)  # noqa
-        elif cmd[0] in ('!', '！'):
-            args = cmd[1:].split()
-            if len(args) > 1:
-                return None
-            elif args[0].lower().startswith('bv') or args[0].lower().startswith('av'):
-                origin_id: str = args[0].strip()
+        elif cmd[:2].lower() in ('av', 'bv'):
+            origin_id: str = cmd
         else:
-            return None
+            return
     else:
-        return None
+        return
 
     try:
         if origin_id is not None:
             info_text, video_cover_url = await get_video_info(origin_id, app, group)
         else:
-            return None
+            return
     except ValueError:
-        return None
+        return
 
     try:
         await app.sendGroupMessage(group, MessageChain.create([
